3684379222e0719c1883d1d0a72d6d729ffb4584
ananthrajsingh <ananthraj.singh@gmail.com>
2019-07-28 14:32:26 +0530

    Add RoomDatabase and tests

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -16,16 +16,36 @@
 
 package com.example.android.trackmysleepquality.database
 
-// TODO (01) Create an abstract class that extends RoomDatabase.
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
-// TODO (02) Declare an abstract value of type SleepDatabaseDao.
+@Database(entities = [SleepDatabase::class], version = 1, exportSchema = false)
+abstract class SleepDatabase: RoomDatabase() {
+    abstract val sleepDatabaseDao: SleepDatabaseDao
 
-// TODO (03) Declare a companion object.
+    companion object {
+        @Volatile
+        var INSTANCE: SleepDatabase? = null
 
-// TODO (04) Declare a @Volatile INSTANCE variable.
+        fun getInstance(context: Context): SleepDatabase {
 
-// TODO (05) Define a getInstance() method with a synchronized block.
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                            context.applicationContext,
+                            SleepDatabase::class.java,
+                            "sleep_history_database"
+                    )
+                            .fallbackToDestructiveMigration()
+                            .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
 
-// TODO (06) Inside the synchronized block:
-// Check whether the database already exists,
-// and if it does not, use Room.databaseBuilder to create it.
+        }
+    }
+}
